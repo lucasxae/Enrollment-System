11994b4335de8978a08f86b8b081cdb0e09e9553
analuizaalvesm <analuizaalvesm@gmail.com>
2024-09-03 18:37:29 -0300

    alterações na classe Curso

--- Implementação/src/main/java/org/example/Curso.java
+++ Implementação/src/main/java/org/example/Curso.java
@@ -2,7 +2,7 @@ package org.example;
 
 import java.util.List;
 
-public class Curso {
+public class Curso implements ICurriculo {
     private String nome;
     private double creditos;
     
@@ -24,5 +24,39 @@ public class Curso {
     public void setDisciplinas(List<Disciplina> disciplinas) {
         this.disciplinas = disciplinas;
     }
+
     private List<Disciplina> disciplinas;
+
+    public void adicionarDisciplina(Disciplina disciplina) {
+        this.disciplinas.add(disciplina);
+    }
+
+    public void removerDisciplina(Disciplina disciplina) {
+        this.disciplinas.remove(disciplina);
+    }
+
+    @Override
+    public String gerarCurriculoSemestral() {
+        StringBuilder curriculo = new StringBuilder();
+        curriculo.append("Curso: ").append(this.nome).append("\n");
+        curriculo.append("Créditos: ").append(this.creditos).append("\n");
+        curriculo.append("Disciplinas:\n");
+    
+        for (Disciplina disciplina : disciplinas) {
+            curriculo.append("\n  Nome: ").append(disciplina.getNome()).append("\n");
+            curriculo.append("  Professores:\n");
+            for (Turma turma : disciplina.getTurmas()) {
+                curriculo.append("    - ").append(turma.getProfessor()).append("\n");
+            }
+            curriculo.append("  Alunos:\n");
+            for (Turma turma : disciplina.getTurmas()) {
+                curriculo.append("    Turma ").append(turma.getCodigo()).append(":\n");
+                for (Aluno aluno : turma.getAlunos()) {
+                    curriculo.append("      - ").append(aluno.getNome()).append("\n");
+                }
+            }
+        }
+        return curriculo.toString();
+    }
+    
 }
