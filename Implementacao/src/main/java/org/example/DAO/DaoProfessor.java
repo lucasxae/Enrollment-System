cc334a3d41a3e3d9b43d439f1a6f2f6a7e36af5d
Yan Cota <yancota10@gmail.com>
2024-09-04 18:37:21 -0300

    Feat entrar no loop professor e registrar no txt

--- Implementacao/src/main/java/org/example/DAO/DaoProfessor.java
+++ Implementacao/src/main/java/org/example/DAO/DaoProfessor.java
@@ -11,7 +11,7 @@ import java.util.List;
 import org.example.Professor;
 
 public class DaoProfessor {
-    public static final String FILE_PATH = "Implementacao//src//main//java//org//example//Data//Disciplinas.txt";
+    private static final String FILE_PATH = "Enrollment-System\\Implementacao\\src\\main\\java\\org\\example\\Data\\Professores.txt";
 
     public void adicionarProfessor(Professor prof) throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter(FILE_PATH));
