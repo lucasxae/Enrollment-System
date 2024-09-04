b504ded442bf374c6be2a4a81f92cb6e7e47b308
analuizaalvesm <analuizaalvesm@gmail.com>
2024-09-03 23:50:53 -0300

    feat: corrigido método para remover disciplina

--- Implementação/src/main/java/org/example/Main.java
+++ Implementação/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,6 +13,7 @@ import org.example.DAO.DaoProfessor;
 import org.example.Enum.TipoDisciplina;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
         DaoAluno daoAluno = new DaoAluno();
@@ -39,7 +41,7 @@ public class Main {
                     break;
                 case 2:
                     if (alunoLogado == null) {
-                        System.out.println("Digite seu nome");
+                        System.out.print("Digite seu nome: ");
                         String nomeAluno = scanner.nextLine();
                         alunoLogado = daoAluno.getByName(nomeAluno);
                         menuAluno(alunoLogado);
@@ -47,19 +49,19 @@ public class Main {
                     break;
                 case 3:
                     if (profLogado == null) {
-                        System.out.println("Digite seu nome");
+                        System.out.print("Digite seu nome: ");
                         String nomeProf = scanner.nextLine();
                         profLogado = daoProfessor.getByNome(nomeProf);
                         menuProfessor(profLogado);
                     }
                     break;
                 case 4:
-                    System.out.println("Saindo.....");
+                    System.out.println("Saindo. . .");
                     scanner.close();
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Opção inválida tente novamente.");
+                    System.out.println("ERRO: Opção inválida! Tente novamente.");
             }
         }
     }
@@ -68,22 +70,22 @@ public class Main {
         for (int i = 1; i <= qtdTurmas; i++) {
             System.out.print("Digite o código da turma " + i + ": ");
             String codigo = s.next();
-    
+
             System.out.print("Insira o nome do professor que irá ministrar a turma " + i + ": ");
             String nomeProf = s.next();
-    
+
             Professor professor = new Professor(null, nomeProf);
             Turma turma = new Turma(codigo, professor, null, null);
-    
+
             turmas.add(turma);
             professor.adicionarTurma(turma);
         }
-    
+
         return turmas;
     }
 
     private static TipoDisciplina obterTipoDisciplina(Scanner s) {
-        System.out.print("Qual o tipo da disciplina? (1 - OBRIGATORIA ou 2 - OPTATIVA): ");
+        System.out.print("Qual o tipo da disciplina? (1 - OBRIGATÓRIA ou 2 - OPTATIVA): ");
         if (s.nextInt() == 1) {
             return TipoDisciplina.OBRIGATORIA;
         } else {
@@ -98,14 +100,15 @@ public class Main {
 
         System.out.println("\n[Curso - O que deseja fazer?]\n");
         System.out.println("1 - Adicionar disciplina");
-        System.out.println("2 - Retirar disciplina");
-        System.err.println("3 - Gerar curriculo");
-        System.out.println("4 - Sair\n");
+        System.out.println("2 - Remover disciplina");
+        System.out.println("3 - Gerar currículo");
+        System.out.println("4 - Listar disciplinas cadastradas");
+        System.out.println("5 - Sair\n");
 
         System.out.print("Selecione uma opção: ");
         int opcao = s.nextInt();
         s.nextLine();
-    
+
         switch (opcao) {
             case 1:
                 try {
@@ -126,42 +129,64 @@ public class Main {
                     Disciplina nova = new Disciplina(true, cursoPrincipal, tipo, turmas, Dnome, ch);
 
                     cursoPrincipal.adicionarDisciplina(nova);
-                    daoDisciplinas.adicionarDisciplinas(nova);
+                    // daoDisciplinas.adicionarDisciplinas(nova);
                 } catch (Exception e) {
-                    System.out.println(e);
                     System.out.println("ERRO: Não foi possivel adicionar uma nova disciplina.");
                 }
                 break;
             case 2:
                 try {
-                    System.out.println("Digite o nome da disciplina que deseja excluir");
-                    Disciplina procurada = daoDisciplinas.getDisciplinaBynome(s.nextLine());
-                    daoDisciplinas.removerDisciplina(procurada.getNome());
-                    cursoPrincipal.removerDisciplina(procurada);
+                    System.out.print("Digite o nome da disciplina que deseja excluir: ");
+                    String nomeDisciplina = s.nextLine();
+
+                    List<Disciplina> disciplinasDoCurso = cursoPrincipal.getDisciplinas();
+
+                    Iterator<Disciplina> iterator = disciplinasDoCurso.iterator();
+                    while (iterator.hasNext()) {
+                        Disciplina disciplina = iterator.next();
+                        if (disciplina.getNome().equals(nomeDisciplina)) {
+                            iterator.remove();
+                            // daoDisciplinas.removerDisciplina(disciplina.getNome());
+                        }
+                    }
                 } catch (Exception e) {
-                    System.out.println("Não foi possivel excluir esta disciplina");
+                    System.out.println("ERRO: Não foi possivel excluir esta disciplina.");
                 }
             case 3:
                 try {
                     cursoPrincipal.gerarCurriculoSemestral();
                 } catch (Exception e) {
                     System.out.println(e);
-                    System.out.println("Não foi possivel gerar curriculo");
+                    System.out.println("ERRO: Não foi possivel gerar o currículo.");
                 }
                 break;
             case 4:
-                System.out.println("Voltando para o menu principal");
+                try {
+                    List<Disciplina> disciplinas = cursoPrincipal.getDisciplinas();
+                    if (disciplinas.isEmpty()) {
+                        System.out.println("ERRO: Não existem disciplinas cadastradas.");
+                    }
+                    for (Disciplina disciplina : disciplinas) {
+                        System.out.println("Disciplinas: ");
+                        System.out.println(disciplina.getNome());
+                    }
+                } catch (Exception e) {
+                    System.out.println("ERRO: Não foi possivel listar as disciplinas.");
+                }
+                break;
+            case 5:
+                System.out.println("Voltando para o menu principal. . .");
                 return;
             default:
-                System.out.println("Opção invalida tente novamente");
+                System.out.println("ERRO: Opção inválida! Tente novamente.");
         }
     }
 
     public static void menuAluno(Aluno aluno) {
         Scanner b = new Scanner(System.in);
-        System.out.println("\nAluno: " + aluno.getNome() + " o que deseja fazer?\n");
+        System.out.println("\n[Aluno: " + aluno.getNome() + " - O que deseja fazer?]\n");
         System.out.println("1- Matricular em uma disciplina\n");
-        System.out.println("2- Cacelar matricula\n");
+        System.out.println("2- Cancelar matricula\n");
         System.out.println("3- Sair\n");
 
         System.out.println("Selecione uma opção");
@@ -182,7 +207,7 @@ public class Main {
                     try {
 
                     } catch (Exception e) {
-                        System.out.println("Não foi possivel cadastrar na disciplina");
+                        System.out.println("ERRO: Não foi possivel cadastrar na disciplina.");
                     }
                 }
                 break;
@@ -203,22 +228,22 @@ public class Main {
                     }
 
                 } catch (Exception e) {
-                    System.out.println("Não foi possivel desmatricular desta disciplina");
+                    System.out.println("ERRO: Não foi possivel cancelar a matrícula desta disciplina.");
                 }
                 break;
             case 3:
-                System.out.println("Voltando para o menu principal");
+                System.out.println("Voltando para o menu principal. . .");
                 return;
             default:
-                System.out.println("Opção invalida tente novamente");
+                System.out.println("ERRO: Opção inválida! Tente novamente.");
         }
     }
 
     public static void menuProfessor(Professor prof) {
         Scanner d = new Scanner(System.in);
-        System.out.println("\nProfessor: \n" + prof.getNome());
-        System.out.println("1- Ver alunos Matriculados\n");
-        System.out.println("2- sair");
+        System.out.println("\n[Professor: \n" + prof.getNome() + " - O que deseja fazer?]\n");
+        System.out.println("1 - Ver alunos Matriculados\n");
+        System.out.println("2 - Sair");
 
         System.out.println("Escolha uma opção");
         int opcao = d.nextInt();
@@ -228,9 +253,9 @@ public class Main {
                 prof.checarAlunos(DisciplinaProc);
                 break;
             case 2:
-                System.out.println("Voltando para o menu principal");
+                System.out.println("Voltando para o menu principal. . .");
             default:
-                System.out.println("Opção invalida tente novamente");
+                System.out.println("ERRO: Opção inválida! Tente novamente.");
 
         }
     }
